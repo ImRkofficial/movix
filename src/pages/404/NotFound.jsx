c8493417b1488ca8e00a29ffa392383514016950
imRkofficial <rkbagra0490@gmail.com>
2024-01-12 22:47:09 +0530

    initial comit

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -1,9 +1,18 @@
-import './style.scss'
+import React from "react";
+
+import "./style.scss";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
 
-export default NotFound
+export default NotFound;
